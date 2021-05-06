eb69681a048bbfa9454955ef88725a70b21aa532
s3740819 <56997194+s3740819@users.noreply.github.com>
2021-05-06 22:05:47 +0700

    Add files via upload

--- MVC/src/main/java/Handler.java
+++ MVC/src/main/java/Handler.java
@@ -4,9 +4,6 @@ import main.java.entity.Drug;
 import main.java.entity.Producers;
 import main.java.service.Service;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.*;
 
 /**
@@ -16,51 +13,6 @@ public class Handler {
     private static Service service = (Service) new AnnotationConfigApplicationContext(AppConfig.class).getBean("service");
     public static List drugs = service.getDrugsList();
     public static List producers = service.getProducersList();
-    public static void setupDatabase(){
-        ArrayList<Drug> drugs = new ArrayList<>();
-        ArrayList<Producers> producers = new ArrayList<>();
-        try {
-            File myObj = new File("message.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                String[] split = data.split("&&");
-                boolean isExist = false;
-                for (Producers value : producers) {
-                    if (value.getId().equals(split[9])) {
-                        isExist = true;
-                        break;
-                    }
-                }
-                if (!isExist) {
-                    Producers producer = new Producers();
-                    producer.setId(split[9]);
-                    producer.setName(split[8]);
-                    producers.add(producer);
-                }
-            }
-            myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                String[] split = data.split("&&");
-                for (Producers producer : producers) {
-                    if (producer.getId().equals(split[9])) {
-                        Drug drug = new Drug(split[0], split[1], split[2], split[3], split[4], split[5], split[6], split[7], producer, split[10]);
-                        drugs.add(drug);
-                        break;
-                    }
-                }
-            }
-            myReader.close();
-        } catch (FileNotFoundException ignored) {}
-
-        for (Producers producer: producers) {
-            service.saveProducers(producer);
-        }
-        for (Drug drug: drugs) {
-            service.saveDrugs(drug);
-        }
-    }
 
     static List deleteDrug(String id){
         return service.deleteDrug(id);
--- MVC/src/main/java/service/Service.java
+++ MVC/src/main/java/service/Service.java
@@ -10,8 +10,11 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by CoT on 10/13/17.
@@ -22,9 +25,53 @@ public class Service {
 
     @Autowired
     private SessionFactory sessionFactory;
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
+
+    public void setupDatabase(){
+        ArrayList<Drug> drugs = new ArrayList<>();
+        ArrayList<Producers> producers = new ArrayList<>();
+        try {
+            File myObj = new File("message.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] split = data.split("&&");
+                boolean isExist = false;
+                for (Producers value : producers) {
+                    if (value.getId().equals(split[9])) {
+                        isExist = true;
+                        break;
+                    }
+                }
+                if (!isExist) {
+                    Producers producer = new Producers();
+                    producer.setId(split[9]);
+                    producer.setName(split[8]);
+                    producers.add(producer);
+                }
+            }
+            myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] split = data.split("&&");
+                for (Producers producer : producers) {
+                    if (producer.getId().equals(split[9])) {
+                        Drug drug = new Drug(split[0], split[1], split[2], split[3], split[4], split[5], split[6], split[7], producer, split[10]);
+                        drugs.add(drug);
+                        break;
+                    }
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException ignored) {}
+
+        for (Producers producer: producers) {
+            sessionFactory.getCurrentSession().save(producer);
+        }
+        for (Drug drug: drugs) {
+            sessionFactory.getCurrentSession().save(drug);
+        }
     }
+
     public List saveDrug(Drug drug){
         List<String> result = new ArrayList<>();
         for (Object o : Handler.drugs) {
@@ -40,9 +87,6 @@ public class Service {
         return result;
     }
 
-    public void saveDrugs(Drug drug){
-        sessionFactory.getCurrentSession().save(drug);
-    }
 
     public List getDrugsList(){
         CriteriaBuilder builder = sessionFactory.getCurrentSession().getCriteriaBuilder();
@@ -158,10 +202,7 @@ public class Service {
         result.add("success");
         return result;
     }
-    public void saveProducers(Producers producers){
 
-        sessionFactory.getCurrentSession().save(producers);
-    }
 
     public List deleteProducer(String id){
         List<String> result = new ArrayList<>();
