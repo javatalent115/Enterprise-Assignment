0be085fffb6a09709599484386b55b5b95ac5b60
Tran Vu Hoang Viet <viettranvuhoang@gmail.com>
2021-05-11 17:17:23 +0700

    Updated

--- MVC/src/main/resources/static/script.js
+++ MVC/src/main/resources/static/script.js
@@ -1,6 +1,6 @@
 let count = 0;
 //if add to cart btn clicked
-$('.cart-btn').on('click', function (){
+$('.cart-btn').on('click', function () {
   let cart = $('.cart-nav');
   // find the img of that card which button is clicked by user
   let imgtodrag = $(this).parent('li').find("img").eq(0);
@@ -19,10 +19,10 @@ $('.cart-btn').on('click', function (){
       'top': cart.offset().top + 20,
       'left': cart.offset().left + 30,
       'width': 35,
-      'height':  35 
+      'height': 35
     }, 1000, 'easeInOutExpo');
 
-    setTimeout(function(){
+    setTimeout(function () {
       count++;
       $(".cart-nav .item-count").text(count);
     }, 1500);
@@ -30,8 +30,30 @@ $('.cart-btn').on('click', function (){
     imgclone.animate({
       'width': 0,
       'height': 0
-    }, function(){
+    }, function () {
       $(this).detach()
     });
   }
+});
+
+localStorage.setItem("more-button-submenu-visible", false)
+var length = document.querySelectorAll(".more-button-submenu-wrapper")
+for (let i = 0; i < length.length; i++) {
+  document.querySelectorAll(".more-button-submenu-item")[i].setAttribute("style", "display:none")
+}
+$(".more-button").click(function () {
+  if (localStorage.getItem("more-button-submenu-visible") == "true") {
+    localStorage.setItem("more-button-submenu-visible", false)
+    $(this).parent("li").children("div").children("div").css("display", "none")
+  }
+  else {
+    localStorage.setItem("more-button-submenu-visible", true)
+    $(this).parent("li").children("div").children("div").css("display", "block")
+
+  }
+});
+
+$(".more-button-submenu-item").click(function (){
+  
+
 });
