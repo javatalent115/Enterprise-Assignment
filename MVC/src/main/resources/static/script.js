289f4e1df1e9d7ff00c27b6ed462929b84199aae
Tran Vu Hoang Viet <viettranvuhoang@gmail.com>
2021-05-15 15:00:17 +0700

    Updated

--- MVC/src/main/resources/static/script.js
+++ MVC/src/main/resources/static/script.js
@@ -1,17 +1,17 @@
+
+
 function checkAccount() {
   if (localStorage.getItem("accountType") == "guest") {
     $("li:nth-child(6)").css("display", "none")
     $("li:nth-child(7)").css("display", "none")
     $(".add-image").css("display", "none")
     $(".cart-nav").css("display", "none")
-    $(".amount").css("display","none")
-    
-
+    $(".amount").css("display", "none")
   }
   else if (localStorage.getItem("accountType") == "user") {
     $("li:nth-child(6)").css("display", "none")
     $(".add-image").css("display", "none")
-    $(".more-button").css("display","none")
+    $(".more-button").css("display", "none")
   }
   else {
     $(".amount").css("display", "none")
@@ -20,18 +20,27 @@ function checkAccount() {
     $(".cart-nav").css("display", "none")
   }
 }
-checkAccount();
 $("#medicine2").on('keyup', function (e) {
   if (e.key === 'Enter' || e.keyCode === 13) {
-      console.log("hello")
-      $(".search-button-2").click()
+    console.log("hello")
+    $(".search-button-2").click()
   }
 });
 
-document.querySelector(".item-count").innerHTML = parseInt(0 + localStorage.getItem("cart"))
-sessionStorage.setItem("abc", 123)
+
+function intinialize() {
+  document.querySelector(".item-count").innerHTML = parseInt(0 + localStorage.getItem("cart"))
+  checkAccount();
+  // checkPagination()
+}
+intinialize()
+
+//Initialize cart-item
+let listItem = localStorage.getItem('cart-item')
+  ? JSON.parse(localStorage.getItem('cart-item'))
+  : []
 //if add to cart btn clicked
-$(document).on("click",".cart-btn", function () {
+$(document).on("click", ".cart-btn", function () {
   console.log("hello")
   let count = localStorage.getItem("cart");
   document.querySelector(".item-count").innerHTML = parseInt(0 + localStorage.getItem("cart"))
@@ -60,6 +69,19 @@ $(document).on("click",".cart-btn", function () {
       count++;
       localStorage.setItem("cart", count)
       $(".cart-nav .item-count").text(localStorage.getItem("cart"));
+      let id = $(this).parent("li").parent("ul").find(".id").text()
+      let name = $(this).parent("li").parent("ul").find(".name").text()
+      let stock = $(this).parent("li").parent("ul").find(".stock").text()
+      let price = $(this).parent("li").parent("ul").find(".price").text()
+      var obj = {
+        id: id,
+        name: name,
+        stock: stock,
+        price: price
+      }
+      listItem.push(obj)
+      var myJSON = JSON.stringify(listItem)
+      localStorage.setItem("cart-item", myJSON)
     }, 1500);
 
     imgclone.animate({
@@ -69,6 +91,7 @@ $(document).on("click",".cart-btn", function () {
       $(this).detach()
     });
   }
+
 });
 
 var length = document.querySelectorAll(".more-button-submenu-wrapper")
@@ -190,25 +213,25 @@ function reset() {
 }
 
 
-async function addAllItem(){
+async function addAllItem() {
   let res = await fetch('http://localhost:8080/api/getDrugs', {
     method: 'POST',
     headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     }
-});
-let all ="";
-if (res.ok) {
+  });
+  let all = "";
+  if (res.ok) {
     let data = await res.json();
     let result = Object.values(data);
     for (let i = 0; i < result.length; i++) {
-        name.push(result[i].split(" -- ")[1]);
-        all += result[i].split(" -- ")[0] + "&&" +result[i].split(" -- ")[1] + "&&"+result[i].split(" -- ")[2] + "&&"+result[i].split(" -- ")[3] + "&&"+result[i].split(" -- ")[4] + "&&"+result[i].split(" -- ")[5]
-            + "&&"+result[i].split(" -- ")[6] + "&&"+result[i].split(" -- ")[7] + "&&"+result[i].split(" -- ")[8] + "&&"+result[i].split(" -- ")[9] + "&&"+result[i].split(" -- ")[10] + "&&"+result[i].split(" -- ")[11]+ "\n";
+      name.push(result[i].split(" -- ")[1]);
+      all += result[i].split(" -- ")[0] + "&&" + result[i].split(" -- ")[1] + "&&" + result[i].split(" -- ")[2] + "&&" + result[i].split(" -- ")[3] + "&&" + result[i].split(" -- ")[4] + "&&" + result[i].split(" -- ")[5]
+        + "&&" + result[i].split(" -- ")[6] + "&&" + result[i].split(" -- ")[7] + "&&" + result[i].split(" -- ")[8] + "&&" + result[i].split(" -- ")[9] + "&&" + result[i].split(" -- ")[10] + "&&" + result[i].split(" -- ")[11] + "\n";
     }
-}
-localStorage.setItem("data", all);
+  }
+  localStorage.setItem("data", all);
   let wrapper = document.querySelector(".medicines")
   let data = localStorage.getItem("data")
   for (let i = 0; i < 10; i++) {
@@ -244,34 +267,34 @@ localStorage.setItem("data", all);
 
 addAllItem()
 
-function removeAllItem(){
-  let x = document.querySelectorAll(".medicine-item").length-1
+function removeAllItem() {
+  let x = document.querySelectorAll(".medicine-item").length - 1
   for (let i = x; i > 0; i--) {
     document.querySelectorAll(".medicine-item")[i].remove()
   }
 }
 
-$(document).on("click",".search-button-2",function(){
+$(document).on("click", ".search-button-2", function () {
   let searchName = document.getElementById("medicine2").value
   document.getElementById("medicine2").value = ""
-  if (searchName == 0){
+  if (searchName == 0) {
     removeAllItem()
-    addAllItem() 
+    addAllItem()
   }
-  else{
+  else {
     let data = localStorage.getItem("data")
     removeAllItem()
     let index = 0
     let found = false
     let lastItem = data.split("\n").length
-    for (let i = 0; i < lastItem ; i++) {
-      if(searchName == data.split("\n")[i].split("&&")[1]){
+    for (let i = 0; i < lastItem; i++) {
+      if (searchName == data.split("\n")[i].split("&&")[1]) {
         index = i
         found = true
         break
       }
     }
-    if (found){
+    if (found) {
       let wrapper = document.querySelector(".medicines")
       wrapper.innerHTML += `<ul class="medicine-item">
         <li class="changeAble id">${data.split("\n")[index].split("&&")[0]}</li>
@@ -301,7 +324,7 @@ $(document).on("click",".search-button-2",function(){
     </ul>`
       checkAccount()
     }
-    else{
+    else {
       console.log("Can not find drug")
     }
   }
@@ -309,18 +332,176 @@ $(document).on("click",".search-button-2",function(){
 
 
 
-$(document).on("click",".page-item",function(){
-  let page = $(this).text()
-  removeAllItem()
+/* * * * * * * * * * * * * * * * *
+ * Pagination
+ * javascript page navigation
+ * * * * * * * * * * * * * * * * */
+
+var Pagination = {
+
+  code: '',
+
+  // --------------------
+  // Utility
+  // --------------------
+
+  // converting initialize data
+  Extend: function (data) {
+    data = data || {};
+    Pagination.size = data.size || 300;
+    Pagination.page = data.page || 30;
+    Pagination.step = data.step || 3;
+  },
+
+  // add pages by number (from [s] to [f])
+  Add: function (s, f) {
+    for (var i = s; i < f; i++) {
+      Pagination.code += '<a class ="page">' + i + '</a>';
+    }
+  },
+
+  // add last page with separator
+  Last: function () {
+    Pagination.code += '<i>...</i><a class = "page">' + Pagination.size + '</a>';
+  },
+
+  // add first page with separator
+  First: function () {
+    Pagination.code += '<a class = "page">1</a><i>...</i>';
+  },
+
+
+
+  // --------------------
+  // Handlers
+  // --------------------
+
+  // change page
+  Click: function () {
+    Pagination.page = +this.innerHTML;
+    Pagination.Start();
+  },
+
+  // previous page
+  Prev: function () {
+    Pagination.page--;
+    if (Pagination.page < 1) {
+      Pagination.page = 1;
+    }
+    Pagination.Start();
+  },
+
+  // next page
+  Next: function () {
+    Pagination.page++;
+    if (Pagination.page > Pagination.size) {
+      Pagination.page = Pagination.size;
+    }
+    Pagination.Start();
+  },
+
+
+
+  // --------------------
+  // Script
+  // --------------------
+
+  // binding pages
+  Bind: function () {
+    var a = Pagination.e.getElementsByTagName('a');
+    for (var i = 0; i < a.length; i++) {
+      if (+a[i].innerHTML === Pagination.page) a[i].className = 'current';
+      a[i].addEventListener('click', Pagination.Click, false);
+    }
+  },
+
+  // write pagination
+  Finish: function () {
+    Pagination.e.innerHTML = Pagination.code;
+    Pagination.code = '';
+    Pagination.Bind();
+  },
+
+  // find pagination type
+  Start: function () {
+    if (Pagination.size < Pagination.step * 2 + 6) {
+      Pagination.Add(1, Pagination.size + 1);
+    }
+    else if (Pagination.page < Pagination.step * 2 + 1) {
+      Pagination.Add(1, Pagination.step * 2 + 4);
+      Pagination.Last();
+    }
+    else if (Pagination.page > Pagination.size - Pagination.step * 2) {
+      Pagination.First();
+      Pagination.Add(Pagination.size - Pagination.step * 2 - 2, Pagination.size + 1);
+    }
+    else {
+      Pagination.First();
+      Pagination.Add(Pagination.page - Pagination.step, Pagination.page + Pagination.step + 1);
+      Pagination.Last();
+    }
+    Pagination.Finish();
+  },
+
+
+
+  // --------------------
+  // Initialization
+  // --------------------
+
+  // binding buttons
+  Buttons: function (e) {
+    var nav = e.getElementsByTagName('a');
+    nav[0].addEventListener('click', Pagination.Prev, false);
+    nav[1].addEventListener('click', Pagination.Next, false);
+  },
+
+  // create skeleton
+  Create: function (e) {
+
+    var html = [
+      '  <a class = "page"><img src="./images/arrow-left.png" alt=""></a>', // previous button
+      '<span ></span>',  // pagination container
+      '  <a class = "page"><img src="./images/arrow-right.png" alt=""></a>'  // next button
+    ];
+
+    e.innerHTML = html.join('');
+    Pagination.e = e.getElementsByTagName('span')[0];
+    Pagination.Buttons(e);
+  },
+
+  // init
+  Init: function (e, data) {
+    Pagination.Extend(data);
+    Pagination.Create(e);
+    Pagination.Start();
+  }
+};
+
+var init = function () {
+  Pagination.Init(document.getElementById('pagination'), {
+    size: 160, // pages size
+    page: 1,  // selected page
+    step: 2   // pages before and after current
+  });
+};
+
+document.addEventListener('DOMContentLoaded', init, false);
+let page = 0
+
+$(document).on("click", ".page", function () {
+  page = document.querySelector(".current").innerHTML
+  console.log(page)
+
   let itemDisplayAtATime = 10
   let data = localStorage.getItem("data")
-  let itemIndex = (page-1)*itemDisplayAtATime
+  let itemIndex = (page - 1) * itemDisplayAtATime
   let wrapper = document.querySelector(".medicines")
-  for (let i = itemIndex; i < itemIndex+itemDisplayAtATime; i++) {
-      if(data.split("\n")[i].split("&&")[0]==0){
-        break
-      }
-      else{
+  for (let i = itemIndex; i < itemIndex + itemDisplayAtATime; i++) {
+    if (data.split("\n")[i].split("&&")[0] == 0) {
+      break
+    }
+    else {
       wrapper.innerHTML += `<ul class="medicine-item">
         <li class="changeAble id">${data.split("\n")[i].split("&&")[0]}</li>
         <li class="changeAble name">${data.split("\n")[i].split("&&")[1]}</li>
@@ -348,8 +529,11 @@ $(document).on("click",".page-item",function(){
         </li>
     </ul>`
     }
-  checkAccount()
-
+    checkAccount()
   }
-
 });
+
+
+
+
+
