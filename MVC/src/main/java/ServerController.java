76f9eb3be4e38ea78557bd22b8605c89eacf1cda
s3740819 <s3740819@rmit.edu.vn>
2021-05-18 11:41:12 +0700

    update

--- MVC/src/main/java/ServerController.java
+++ MVC/src/main/java/ServerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin("http://localhost:5500")
+@CrossOrigin("http://localhost:8080")
 public class ServerController {
 
     @PostMapping(value = "/api/getDrugs")
