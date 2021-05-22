c04a6c5be9b44d2a7ebb6db9af006ac513283b22
Việt Trần Vũ Hoàng <zieeettt@Viets-MacBook-Air.local>
2021-05-23 02:07:25 +0700

    updated

--- MVC/target/classes/static/drug-infomation.js
+++ MVC/target/classes/static/drug-infomation.js
@@ -11,7 +11,43 @@ function initialize(){
     $(".country").text(drug.country)
     $(".group").text(drug.group)
     $(".type").text(drug.type)
-    $(".drug-image").attr("src","./images/blank-image.png")
+    $(".drug-image").attr("src",returnImage(drug.name))
 }
 
 initialize()
+
+function returnImage(name){
+    if (name.includes("Panadol") ){
+        return "./images/others/panadol.png"
+    }
+    else if(name.includes("EtonciB1")){
+        return "./images/others/EtonciB1.png"
+    }
+    else if(name.includes("Berberin")){
+        return "./images/others/Berberin.png"
+    }
+    else if(name.includes("Condova")){
+        return "./images/others/Condova.png"
+    }
+    else if(name.includes("Diazepam")){
+        return "./images/others/Diazepam.png"
+    }
+    else if(name.includes("Dodevifort")){
+        return "./images/others/Dodevifort.png"
+    }
+    else if(name.includes("Felodipin Stada")){
+        return "./images/others/Felodipin Stada.png"
+    }
+    else if(name.includes("Fluopas")){
+        return "./images/others/Fluopas.png"
+    }
+    else if(name.includes("Mifenan")){
+        return "./images/others/Mifenan.png"
+    }
+    else if(name.includes("Paracetamol")){
+        return "./images/others/Paracetamol.png"
+    }
+    else{
+        return "./images/blank-image.png"
+    }
+}
